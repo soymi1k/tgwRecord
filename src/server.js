3c209f1581b4a37ab53216f48166499f31ab56ca
Chris <cochsenreither@protonmail.com>
2021-06-25 09:44:35 -0700

    video canvases 320x200 px

--- src/server.js
+++ src/server.js
@@ -11,8 +11,11 @@ configs.cameras.forEach(c => {
     new Stream({
         streamUrl: 'rtsp://' + c.username + ':' + c.password + '@' + c.ip_address + ':554/live/ch1',
         wsPort: c.websocket,
+        width: 320,
+        height: 200,
         ffmpegOptions: {
-            '-r': 30
+            '-r': 30,
+            '-s': '320x200'
         }
     })
 });
